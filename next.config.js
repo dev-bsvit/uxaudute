012373a6ecb7bfd7b30b1634b621e766746ea98e
dev-bsvit <dev.bsvit@gmail.com>
2025-09-02 12:33:09 +0200

    Fix favicon with inline SVG data URL

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react']
+  }
+}
 
 module.exports = nextConfig
